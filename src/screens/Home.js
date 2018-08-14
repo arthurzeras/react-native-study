ef7f337644e77445d7cbe5dc0416e3ca3b890ff9
arthurzeras <arthurolvmorais@gmail.com>
2018-08-14 16:34:10 -0300

    Listando cotações

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,21 +13,29 @@ export default class Home extends React.Component {
   async componentDidMount() {
     const res = await fetch('https://api.promasters.net.br/cotacao/v1/valores')
     const responseJson = await res.json()
-    console.log(responseJson)
-    this.setState({
-      cotacoes: responseJson.valores
-    })
-  }
 
-  montarLista () {
-    return Object.keys(this.state.cotacoes)
+    let cotacoes = []
+    
+    for (c in responseJson.valores) {
+      cotacoes.push({
+        nome: responseJson.valores[c].nome,
+        valor: responseJson.valores[c].valor
+      })
+    }
+
+    this.setState({
+      cotacoes: cotacoes
+    })
   }
 
   render() {
     return (
-      <Text>
-        Dólar: { this.state.cotacoes ? this.state.cotacoes.USD.valor : '' }
-      </Text>
+      <View>
+        { this.state.cotacoes
+          ? this.state.cotacoes.map((cot, idx) => <Text key={idx}>{cot.nome}: {cot.valor}</Text>)
+          : ''
+        }
+      </View>
     );
   }
 }
